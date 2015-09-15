406fe17adee188f9decfbaa8d21f370ea62306f7
Daniel Kristiyanto <danielkr@uw.edu>
2015-09-15 13:06:20 -0700

    start-mac

--- Install and Run /MAC/start-mac.sh
+++ Install and Run /MAC/start-mac.sh
@@ -15,9 +15,9 @@ export DockerImage="kristiyanto/guidock"
 # Script starts here ---
 docker-machine start $(docker-machine ls | awk 'FNR == 2 {print $1}')
 echo "Launching Socat to bind the X11 services."
-#socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
+socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
 echo "Launching XQuartz"
-#open -a xquartz &
+open -a xquartz &
 eval $(docker-machine env $(docker-machine ls | awk 'FNR == 2 {print $1}'))
 docker pull $DockerImage
 docker run -ti -e DISPLAY=$MacIP:0 $DockerImage
