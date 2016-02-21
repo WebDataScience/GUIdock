3b0f851026fb3deeadd4f84df995463f04485a9d
Varun Mittal <varunmittal91@gmail.com>
2016-02-21 18:07:24 -0500

    Added delayed script interface, rewritten cytoscape_3_3 to create considerably smaller images. On first run cytoscape gets deflated

--- BaseImages/base_image_generator.py
+++ BaseImages/base_image_generator.py
@@ -19,13 +19,13 @@ command_dict = {
     "add"        : ["add", process_add]
 }
 
-def load_dep(dep, commands=[], expose_ports=[], entry_points=[]):
+def load_dep(dep, commands=[], expose_ports=[], entry_points=[], scripts=[]):
     path = "lib/%s.json" % dep
     json_data = load(open(path, "rb"))
     depends   = json_data.get('depends', [])
     commands.append("#%s.json" % dep)
-    for dep in depends:
-        load_dep(dep, commands)
+    for _dep in depends:
+        load_dep(_dep, commands, scripts=scripts)
     for command in json_data['commands']:
         try:
             command,arg = command
@@ -46,8 +46,21 @@ def load_dep(dep, commands=[], expose_ports=[], entry_points=[]):
     entry_point = json_data.get("entrypoint", [])
     if entry_point:
         entry_points.append(entry_point)
+    script = json_data.get("script")
+    if script:
+        from uuid import uuid4
+        # Generate uuid4 name for first time setup script
+        file_name = str(uuid4())
+        arg = "%s /%s" % (script,file_name)
+        command,arg = process_add("add", arg, dep)
+        command = "%s %s" % (command,arg)
+        commands.append(command)
+        command_str = "/%s" % file_name
+        scripts.append("chmod +x %s" % command_str)
+        scripts.append(command_str)
+        scripts.append("rm -rf %s" % command_str)
 
-    return commands,expose_ports,entry_points
+    return commands,expose_ports,entry_points,scripts
 
 def load_json(json_path, dockerfile=None):
     import json
@@ -68,20 +81,28 @@ def load_json(json_path, dockerfile=None):
     sub_commands = []
     expose_ports = []
     entry_points = []
+    scripts      = []
     for lib in data.get('libs', []):
-        sub_commands,expose_ports,entry_points = load_dep(lib)
+        sub_commands,expose_ports,entry_points,scripts = load_dep(lib)
     dockerfile.extend(sub_commands)
     dockerfile.extend([
         "RUN apt-get purge software-properties-common -y --force-yes",
         "RUN apt-get -y autoclean",
         "RUN apt-get -y autoremove",
         "RUN rm -rf /var/lib/apt/lists/*",
+        "RUN rm -rf /tmp/*",
+        "RUN rm -rf /var/tmp/*"
     ])
     if expose_ports:
         dockerfile.append("EXPOSE %s" % " ".join(expose_ports))
+
     if entry_points:
         entry_point = entry_points[-1]
-        dockerfile.append("ENTRYPOINT %s" % json.dumps(entry_point))
+        scripts.append(" ".join(entry_point))
+    exec_command = 'echo -e "%s" >> /entrypoint.sh' % ("\\n".join(["#!/bin/bash"]+scripts))
+    dockerfile.append("RUN bash -c '%s'" % exec_command)
+    dockerfile.append("RUN chmod +x /entrypoint.sh")
+    dockerfile.append("ENTRYPOINT %s" % json.dumps(["/entrypoint.sh"]))
     
     dockerfile = "\n".join(dockerfile)
     return dockerfile
