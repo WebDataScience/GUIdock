8b87bc0ad506e8643ad166bbc2c76faac7520d6d
Varun Mittal <varunmittal91@gmail.com>
2016-03-18 10:26:58 -0400

    Adding basic google login service, still very experimental

--- BaseImages/lib/broker_base/init.sh
+++ BaseImages/lib/broker_base/init.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
 cd / && tar -xf /broker.tar.gz && rm broker.tar.gz
+cd /broker && python manage.py syncdb --noinput
+
+while getopts ":u:p:" opt; do
+  case $opt in
+    u)
+      echo "-a was triggered, Parameter: $OPTARG" >&2
+      echo $OPTARG >  /google_cred
+      ;;
+    p)
+      echo "-a was triggered, Parameter: $OPTARG" >&2
+      echo $OPTARG >> /google_cred
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
 
 chmod +x /broker/init.sh
 /broker/init.sh
